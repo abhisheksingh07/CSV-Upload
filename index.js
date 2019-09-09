dcea1015a096ced227ffdab3aa46e02e4653f9cb
Abhishek <abhishek.singh07@hotmail.com>
2019-09-09 13:19:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
-var express = require('express');
-var session = require('express-session');
-var csv = require("fast-csv");
-var json2csv = require('json2csv');
-var mongoose = require('mongoose');
-var ssn ;
-var Part = require("../model/part");
-var Machine = require("../model/MachineName");
-var Sub = require("../model/MachineSubcategory");
+let express = require('express');
+let session = require('express-session');
+let csv = require("fast-csv");
+let json2csv = require('json2csv');
+const mongoose = require('mongoose');
+let ssn ;
+let Part = require("../model/part");
+let Machine = require("../model/MachineName");
+let Sub = require("../model/MachineSubcategory");
 
 module.exports = function(app) {
 app.get('/', function(req, res) {
@@ -16,90 +16,54 @@ app.get('/', function(req, res) {
 		res.render('product');
 		
     }); 
-	
-
-	
+		
 app.post('/', function(req,res){
 		 
-		 console.log(req.files);
-		
-		if (!req.files)
-		return res.status(400).send('No files were uploaded.');
+	console.log(req.files);	
+	if (!req.files)
+	return res.status(400).send('No files were uploaded.');
 	
-	var partFile = req.files.file;
+	let partFile = req.files.file;
 	
 
-	var parts = [];
-		
-	// csv
-	 // .fromString(partFile.data.toString(), {
-		 // headers: true,
-		 // ignoreEmpty: true
-	 // })
-	 // .on("data", function(data){
-		 // data['_id'] = new mongoose.Types.ObjectId();
-		 
-		 // parts.push(data);
-	 // })
-	 // .on("end", function(){
-		// Part.create(parts, function(err, documents) {
-			// if (err) throw err;
-			
-			// res.send(parts.length + ' authors have been successfully uploaded.');
-		 // });
-	 // });
-	
-	var newMachine = new Machine({
+	let parts = [];
+	let newMachine = new Machine({
 		Category:req.body.Category
 	});
-   let b = [];
+   	let b = [];
 	newMachine.save( function(err,data){
 		if(err) throw err;
-		var a = data._id;
-		csv
-	 .fromString(partFile.data.toString(), {
+		let a = data._id;
+		//csv file upload function
+		csv.fromString(partFile.data.toString(), {
 		 headers: true,
 		 ignoreEmpty: true
-	 })
-	 .on("data", function(data){
+	 	}).on("data", function(data){
 		 data['_id'] = new mongoose.Types.ObjectId();
-		 
 		 parts.push(data);
-	 })
-	 .on("end", function(){
-		Part.create(parts, function(err, result) {
-			if (err) throw err;
+	 }).on("end", function(){
+		Part.create(parts, function(err, result) { //this function creates the part collection in db
+		if (err) throw err;
 		console.log("Hello"+result+"HEllo dozers");
-		for(var i =0; i<result.length; i++){
-			b.push(result[i]._id);
-		
+		for(let i =0; i<result.length; i++){
+		b.push(result[i]._id);
 		}
-		
-        console.log(b);
-				
-			var newSub = new Sub({
+		console.log(b);
+		let newSub = new Sub({
 		ModelName:req.body.ModelName,
 		MainCategory:a,
 		Spid:b
-	})
-			newSub.save();
-			
-			
-			res.send(parts.length + ' authors have been successfully uploaded.');
-			 var modelName = Sub.findOne({"ModelName": req.body.ModelName}, function(err,data){
-			 console.log(data);
-			 var a = data.ModelName;
-			 console.log(a);
-			 
+		})
+		newSub.save();
+		res.send(parts.length + ' authors have been successfully uploaded.');
+		let modelName = Sub.findOne({"ModelName": req.body.ModelName}, function(err,data){
+		console.log(data);
+		let a = data.ModelName;
+		console.log(a);
 		 });
 		 });
 	 });
 		
 	});
-	
-	
-})	
-	
-	
-
-};
+	})	
+	};
